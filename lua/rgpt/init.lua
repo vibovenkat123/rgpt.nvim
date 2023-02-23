5abd8d00d13074c6f6e1b7aaa54292b8b185c559
vibovenkat123 <vibovenkat@gmail.com>
2023-02-22 22:30:52 -0600

    fix: remove unused function

--- lua/rgpt/init.lua
+++ lua/rgpt/init.lua
@@ -1,14 +1,4 @@
 print("loaded rgpt")
-function os.capture(cmd, raw)
-  local f = assert(io.popen(cmd, 'r'))
-  local s = assert(f:read('*a'))
-  f:close()
-  if raw then return s end
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  s = string.gsub(s, '[\n\r]+', ' ')
-  return s
-end
 function Read_file()
     local current_path = vim.api.nvim_buf_get_name(0)
     local handle = io.popen(string.format("rgpt --input \"$(git diff %s)\"", current_path))
