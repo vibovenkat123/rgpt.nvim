fb8e01f7905d5daab95a466cdf437615f75779e0
vibovenkat123 <vibovenkat@gmail.com>
2023-02-24 22:33:43 -0600

    feat: make the plugin open the new file in a split window

--- lua/rgpt/init.lua
+++ lua/rgpt/init.lua
@@ -7,5 +7,23 @@ function Read_file()
         print("The current file has no git diff")
         return
     end
-    print(output)
+    local filename = Write_File(output)
+    vim.cmd('vsplit')
+    local win = vim.api.nvim_get_current_win()
+    local buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_win_set_buf(win, buf)
+    vim.cmd("edit " .. filename)
+    os.remove(filename)
+end
+function Write_File(output)
+    local file = io.open("/tmp/rgpt-output", "w+")
+    if file ~= nil then
+        io.output(file)
+        io.write(output)
+        io.close(file)
+        return "/tmp/rgpt-output"
+    else
+        print("Error writing file")
+        os.exit(1)
+    end
 end
